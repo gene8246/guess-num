9d408016bf92738ff3a9d4aadef7f2142c9740f1
Jeffrey <gene8246@gmail.com>
2022-08-03 00:19:55 +0900

    final

--- guess-num.py
+++ guess-num.py
@@ -1,5 +1,10 @@
+start = input('please decide the start number') #決定隨機數字開始值
+end = input('pease diced the end number') #決定隨機數字結束值
+start = int(start)
+end = int(end)
+
 import random
-r = random.randint(1,100)
+r = random.randint(start, end)
 count = 0
 while True:
 	count = count + 1 # count += 1 
